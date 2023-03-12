4f4658c66b9a743168495b59f4e2dea1b33e16a2
Fernando Reza Campos <fernandorezacampos@gmail.com>
2023-03-11 19:04:37 -0600

    Add algortihms (only metadata)

---
+++ src/components/snake_game.py
@@ -0,0 +1,9 @@
+# This file will contain the SnakeGame class which will be used to test the Evolutive Algorithms performance
+
+
+
+
+import pygame  # Used to create the game window and handle the events
+
+
+
